b652ed70149451b1a977233c4e9f0f3739ed4653
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2024-01-01 08:19:37 +0900

    Update yolox2.py

--- models/detectors/yolox2/yolox2.py
+++ models/detectors/yolox2/yolox2.py
@@ -2,15 +2,15 @@
 import torch
 import torch.nn as nn
 
+# --------------- External components ---------------
+from utils.misc import multiclass_nms
+
 # --------------- Model components ---------------
 from .yolox2_backbone import build_backbone
 from .yolox2_neck import build_neck
 from .yolox2_pafpn import build_fpn
 from .yolox2_head import build_det_head
 from .yolox2_pred import build_pred_layer
-
-# --------------- External components ---------------
-from utils.misc import multiclass_nms
 
 
 # YOLOX2
@@ -26,44 +26,43 @@ class YOLOX2(nn.Module):
                  deploy      = False,
                  nms_class_agnostic = False):
         super(YOLOX2, self).__init__()
-        # ---------------------- Basic Parameters ----------------------
-        self.cfg = cfg
-        self.device = device
-        self.strides = cfg['stride']
-        self.num_classes = num_classes
-        self.trainable = trainable
-        self.conf_thresh = conf_thresh
-        self.nms_thresh = nms_thresh
-        self.num_levels = len(self.strides)
-        self.num_classes = num_classes
-        self.topk = topk
-        self.deploy = deploy
+        # ------------------------- Basic parameters  ---------------------------
+        self.cfg                = cfg                  # Model configuration file
+        self.device             = device               # cuda or cpu
+        self.num_classes        = num_classes          # number of classes
+        self.trainable          = trainable            # training mark
+        self.conf_thresh        = conf_thresh          # score threshold
+        self.nms_thresh         = nms_thresh           # NMS threshold
+        self.num_levels         = len(self.strides)
+        self.topk               = topk                 # topk
+        self.strides            = cfg['stride']
+        self.deploy             = deploy
         self.nms_class_agnostic = nms_class_agnostic
         self.head_dim = round(256 * cfg['width'])
         
-        # ---------------------- Network Parameters ----------------------
-        ## ----------- Backbone -----------
-        self.backbone, feat_dims = build_backbone(cfg)
+        # ----------------------- Model network structure -----------------------
+        ## Backbone network
+        self.backbone, feats_dim = build_backbone(cfg)
 
         ## ----------- Neck: SPP -----------
-        self.neck = build_neck(cfg, feat_dims[-1], feat_dims[-1])
-        feat_dims[-1] = self.neck.out_dim
+        self.neck = build_neck(cfg, feats_dim[-1], feats_dim[-1])
+        feats_dim[-1] = self.neck.out_dim
         
-        ## ----------- Neck: FPN -----------
-        self.fpn = build_fpn(cfg, feat_dims, out_dim=self.head_dim)
+        ## Feature Pyramid
+        self.fpn = build_fpn(cfg, feats_dim, out_dim=self.head_dim)
         self.fpn_dims = self.fpn.out_dim
 
-        ## ----------- Heads -----------
+        ## Detection head
         self.det_heads = build_det_head(cfg, self.fpn_dims, self.head_dim, self.num_levels)
 
-        ## ----------- Preds -----------
+        ## Prediction layer
         self.pred_layers = build_pred_layer(cls_dim     = self.det_heads.cls_head_dim,
                                             reg_dim     = self.det_heads.reg_head_dim,
                                             strides     = self.strides,
                                             num_classes = num_classes,
                                             num_coords  = 4,
                                             num_levels  = self.num_levels)
-
+        
     ## post-process
     def post_process(self, cls_preds, box_preds):
         """
@@ -88,11 +87,11 @@ class YOLOX2(nn.Module):
             # torch.sort is actually faster than .topk (at least on GPUs)
             predicted_prob, topk_idxs = scores_i.sort(descending=True)
             topk_scores = predicted_prob[:num_topk]
-            topk_idxs = topk_idxs[:num_topk]
+            topk_idxs   = topk_idxs[:num_topk]
 
             # filter out the proposals with low confidence score
             keep_idxs = topk_scores > self.conf_thresh
-            scores = topk_scores[keep_idxs]
+            scores    = topk_scores[keep_idxs]
             topk_idxs = topk_idxs[keep_idxs]
 
             anchor_idxs = torch.div(topk_idxs, self.num_classes, rounding_mode='floor')
@@ -116,13 +115,14 @@ class YOLOX2(nn.Module):
         # nms
         scores, labels, bboxes = multiclass_nms(
             scores, labels, bboxes, self.nms_thresh, self.num_classes, self.nms_class_agnostic)
-        
+
         return bboxes, scores, labels
+
 
     # ---------------------- Main Process for Inference ----------------------
     @torch.no_grad()
     def inference_single_image(self, x):
-        # ---------------- Backbone ----------------
+        # Backbone network
         pyramid_feats = self.backbone(x)
 
         # ---------------- Neck: SPP ----------------
@@ -131,7 +131,7 @@ class YOLOX2(nn.Module):
         # ---------------- Neck: PaFPN ----------------
         pyramid_feats = self.fpn(pyramid_feats)
 
-        # ---------------- Heads ----------------
+        # Detection head
         cls_feats, reg_feats = self.det_heads(pyramid_feats)
 
         # ---------------- Preds ----------------
@@ -143,23 +143,26 @@ class YOLOX2(nn.Module):
         if self.deploy:
             cls_preds = torch.cat(all_cls_preds, dim=1)[0]
             box_preds = torch.cat(all_box_preds, dim=1)[0]
-            scores = cls_preds.sigmoid()
-            bboxes = box_preds
+            scores    = cls_preds.sigmoid()
+            bboxes    = box_preds
             # [n_anchors_all, 4 + C]
             outputs = torch.cat([bboxes, scores], dim=-1)
 
             return outputs
         else:
-            # post process
-            bboxes, scores, labels = self.post_process(all_cls_preds, all_box_preds)
-        
+            # Post-processing
+            bboxes, scores, labels = self.post_process(
+                all_cls_preds, all_box_preds)
+            
             return bboxes, scores, labels
 
+
+    # ---------------------- Main Process for Training ----------------------
     def forward(self, x):
         if not self.trainable:
             return self.inference_single_image(x)
         else:
-            # ---------------- Backbone ----------------
+            # Backbone network
             pyramid_feats = self.backbone(x)
 
             # ---------------- Neck: SPP ----------------
@@ -168,10 +171,10 @@ class YOLOX2(nn.Module):
             # ---------------- Neck: PaFPN ----------------
             pyramid_feats = self.fpn(pyramid_feats)
 
-            # ---------------- Heads ----------------
+            # Detection head
             cls_feats, reg_feats = self.det_heads(pyramid_feats)
 
             # ---------------- Preds ----------------
             outputs = self.pred_layers(cls_feats, reg_feats)
             
-            return outputs 
+            return outputs
